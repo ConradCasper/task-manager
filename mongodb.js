8172785a2f701aff5604be51a4ec193c0e6f498d
Conrad <cnrdcasper@gmail.com>
2020-03-26 15:58:59 -0400

    used insertMany to insert 3 documents to the tasks collection

--- mongodb.js
+++ mongodb.js
@@ -13,9 +13,56 @@ MongoClient.connect(connectionURL,{ useNewUrlParser: true, useUnifiedTopology: t
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Conrad',
-        age: 28
-    })
+//     db.collection('users').insertOne({
+//         name: 'Conrad',
+//         age: 28
+//     }, (error, result) => {
+//         if (error) {
+//             return console.log('Unable to insert user')
+//         }
+//         console.log(result.ops)
+//     })
+
+
+// db.collection('users').insertMany([
+//     {
+//         name: 'Jen',
+//         age: 28
+//     },
+//     {
+//         name: 'Gunther',
+//         age: 27
+//     }
+// ], (error, result) => {
+//     if (error) {
+//         return console.log('Unable to insert documents!')
+//     }
+
+//     console.log(result.ops)
+// })
+
+db.collection('tasks').insertMany([
+    {
+        description: 'walk roscoe',
+        completed: true    
+    },
+    {
+        description: 'apply to 15 jobs',
+        completed: false
+    },
+    {
+        description: 'take shower',
+        completed: true
+    }
+], (error, result) => {
+    if (error) {
+        return console.log('Unable to insert documents!')
+    }
+
+    console.log(result.ops)
 })
+
+
+})
+
 
