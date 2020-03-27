f9488e7ebc071885ee273efacea9250b050bd5da
Conrad <cnrdcasper@gmail.com>
2020-03-27 10:39:24 -0400

    explored how mongodb creates unique ids

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,13 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL,{ useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -13,15 +16,16 @@ MongoClient.connect(connectionURL,{ useNewUrlParser: true, useUnifiedTopology: t
 
     const db = client.db(databaseName)
 
-//     db.collection('users').insertOne({
-//         name: 'Conrad',
-//         age: 28
-//     }, (error, result) => {
-//         if (error) {
-//             return console.log('Unable to insert user')
-//         }
-//         console.log(result.ops)
-//     })
+    // db.collection('users').insertOne({
+    //     _id: id,
+    //     name: 'Vikram',
+    //     age: 26
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+    //     console.log(result.ops)
+    // })
 
 
 // db.collection('users').insertMany([
@@ -41,26 +45,26 @@ MongoClient.connect(connectionURL,{ useNewUrlParser: true, useUnifiedTopology: t
 //     console.log(result.ops)
 // })
 
-db.collection('tasks').insertMany([
-    {
-        description: 'walk roscoe',
-        completed: true    
-    },
-    {
-        description: 'apply to 15 jobs',
-        completed: false
-    },
-    {
-        description: 'take shower',
-        completed: true
-    }
-], (error, result) => {
-    if (error) {
-        return console.log('Unable to insert documents!')
-    }
+// db.collection('tasks').insertMany([
+//     {
+//         description: 'walk roscoe',
+//         completed: true    
+//     },
+//     {
+//         description: 'apply to 15 jobs',
+//         completed: false
+//     },
+//     {
+//         description: 'take shower',
+//         completed: true
+//     }
+// ], (error, result) => {
+//     if (error) {
+//         return console.log('Unable to insert documents!')
+//     }
 
-    console.log(result.ops)
-})
+//     console.log(result.ops)
+// })
 
 
 })
