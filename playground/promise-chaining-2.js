3ce3aad8df4e34ed823a4c2339fde57a1afd7bc6
Conrad <cnrdcasper@gmail.com>
2020-04-02 18:15:51 -0400

    used async await to create a function that takes an id and deletes a document from a mongoDB database.

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e84ba7d3401214cc58019eb').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5e84d714e3e24a509ccb4c12').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+   const taskToDelete =  await Task.findByIdAndDelete(id)
+   const count = await Task.countDocuments({ completed: false })
+   return count
+}
+
+deleteTaskAndCount('5e84d6fde3e24a509ccb4c10').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
